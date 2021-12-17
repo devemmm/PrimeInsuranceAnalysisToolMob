7bcd948ab5a4cdc9fdabc23ec2933bb0f6d4861a
devemmm <djntivuguruzwaemmanuel@gmail.com>
2021-12-17 21:05:46 +0200

    Created a new Expo app

---
+++ src/screens/SurveyScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SurveyScreen = () => {
+    return (
+        <View>
+            <Text>Survey Screen</Text>
+        </View>
+    )
+}
+
+export default SurveyScreen
